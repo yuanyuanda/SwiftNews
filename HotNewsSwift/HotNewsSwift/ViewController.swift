b83afdec7191c9cacf4d6b00e13944c49312f08c
yuanyuanda <2570465122@qq.com>
2017-07-14 14:45:19 +0800

    create swift pro

---
+++ HotNewsSwift/HotNewsSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HotNewsSwift
+//
+//  Created by 邢媛媛 on 17/7/14.
+//  Copyright © 2017年 邢媛媛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
